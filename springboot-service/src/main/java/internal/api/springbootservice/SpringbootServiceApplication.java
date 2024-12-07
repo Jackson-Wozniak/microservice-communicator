2fb4efd3f756e917813e21acf762d22e0e2c7771
Jackson Wozniak <jjw30@comcast.net>
2024-12-07 12:23:24 -0500

    initial files

---
+++ springboot-service/src/main/java/internal/api/springbootservice/SpringbootServiceApplication.java
@@ -0,0 +1,13 @@
+package internal.api.springbootservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootServiceApplication.class, args);
+    }
+
+}
