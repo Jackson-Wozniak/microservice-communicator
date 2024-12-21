be1aabde7cf37684d9233f31522bccfbe457f27b
Jackson Wozniak <jjw30@comcast.net>
2024-12-21 17:04:04 -0500

    implement the spring boot request payload that will be sent from dotnet service

--- dotnet-service/dotnet-service/src/Clients/SpringBootHttpClient.cs
+++ dotnet-service/dotnet-service/src/Clients/SpringBootHttpClient.cs
@@ -4,9 +4,11 @@ namespace dotnet_service.Clients;
 
 public class SpringBootHttpClient
 {
+    private static readonly string SpringBootSendMessage = "http://localhost:8080/api/v1/conversations";
+    
     public void SendNextMessage(Message message)
     {
-        var newMessage = new SpringBootMessageRequest();
+        var newMessage = SpringBootMessageRequest.nextMessage(message);
         
         //send to spring boot
     }
--- dotnet-service/dotnet-service/src/Clients/SpringBootMessageRequest.cs
+++ dotnet-service/dotnet-service/src/Clients/SpringBootMessageRequest.cs
@@ -1,6 +1,26 @@
-﻿namespace dotnet_service.Clients;
+﻿using dotnet_service.Entities;
+
+namespace dotnet_service.Clients;
 
 public class SpringBootMessageRequest
 {
+    public long MessageId { get; set; }
     
+    public string ConversationName { get; set; }
+    
+    public string TimestampSent { get; set; }
+
+    private SpringBootMessageRequest()
+    {
+        
+    }
+
+    public static SpringBootMessageRequest nextMessage(Message message)
+    {
+        var newMessage = new SpringBootMessageRequest();
+        newMessage.MessageId = message.MessageId + 1;
+        newMessage.ConversationName = message.ConversationName;
+        newMessage.TimestampSent = DateTime.Now.ToString("hh:mm:ss");
+        return newMessage;
+    }
 }
