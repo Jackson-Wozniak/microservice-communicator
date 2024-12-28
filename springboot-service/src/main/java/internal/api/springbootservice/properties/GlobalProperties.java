9f66ad88550f7b7e9bd95e7a678704de6bdf7f57
Jackson Wozniak <jjw30@comcast.net>
2024-12-28 14:59:05 -0500

    update Dotnet URL

--- springboot-service/src/main/java/internal/api/springbootservice/properties/GlobalProperties.java
+++ springboot-service/src/main/java/internal/api/springbootservice/properties/GlobalProperties.java
@@ -4,7 +4,7 @@ public class GlobalProperties {
 
     public static final String SPRING_BOOT_MESSAGE_NAME = "Sprint Boot Conversation";
     public static final String DOTNET_CLIENT_ROOT = "";
-    public static final String DOTNET_CLIENT_NEW_MESSAGE = "";
+    public static final String DOTNET_CLIENT_NEW_MESSAGE = "http://localhost:17428/api/Conversations";
     public static final String DOTNET_CLIENT_DELETE_CONVERSATION = "";
     public static final String DOTNET_CLIENT_START_CONVERSATION = "";
 }
